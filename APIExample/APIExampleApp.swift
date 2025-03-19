698a9c163ac34de8f5a4c3f7b8e75be3cf87fff0
Naif Ghannam <dhdfaui@gmail.com>
2025-03-19 13:40:17 +0300

    Initial Commit

---
+++ APIExample/APIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIExampleApp.swift
+//  APIExample
+//
+//  Created by Mac on 19/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct APIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
